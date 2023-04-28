acca230082d6c3c3fc54a5eaeaf42137e31880bd
DaniJ09 <danielajaraga@gmail.com>
2023-04-28 17:37:21 -0500

    Salir, cookies y partials

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -1,11 +1,38 @@
 import { Router } from "express";
-
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 const dash= Router();
 
 
 dash.get('/inicio', (req, res)=>{
-res.render('dash');
+
+        if (req.cookies.ckjs) {
+            
+
+            try {
+                const token = jwt.verify(req.cookies.ckjs, process.env.SECRET_KEY);
+
+                res.render("dash", {"nombre": token.nombre,
+                                    "foto": token.foto
+                                    });
+
+            } catch (error) {
+
+                res.redirect("/")
+
+            }
+
+        }else{
+        res.redirect("/");
+        }
+
+
 /* console.log('Logueo Exitoso'); */
 });
 
+dash.get('/salir', (req,res)=>{
+    res.clearCookie("ckjs");
+    res.redirect('/');
+})
 export default dash;
--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const LoginRoute = Router();
 
@@ -8,9 +12,23 @@ LoginRoute.get("/google", (req,res)=>{
    const id = (req.user.id);
    const name = req.user.displayName;
    const email = req.user.emails[0].value;
+   const fotos =req.user.photos[0].value;
 
-   res.render("backoffice",{nombre:" "});
+   const payload = {
+      nombre : name,
+      correro : email,
+      foto : fotos
+   }
+   
+   
+   const token= jwt.sign(payload,process.env.SECRET_KEY,{"expiresIn": process.env.EXPIRE_TOKEN});
+   console.log(token, payload);
+
+   res.cookie("ckjs", token);
+
+   res.redirect('/dashboard/inicio');
+   /* res.render("backoffice",{nombre:" "}); */
 });
 
 
- export default LoginRoute;
+ export default LoginRoute ;
