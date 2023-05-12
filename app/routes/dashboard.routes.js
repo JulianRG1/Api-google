cf33b256cb1075ab6e717e7d4ed6ff1bbdae4186
JulianGuerrero <guerreroydragonxxs@gmail.com>
2023-05-12 12:18:40 -0500

    ..

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -58,6 +58,57 @@ dash.get('/usuario', (req,res)=>{
     } 
 });
 
+dash.get('/productos', (req, res)=>{
+
+    if (req.cookies.ckjs) {
+            
+
+        try {
+            const token = jwt.verify(req.cookies.ckjs, process.env.SECRET_KEY);
+
+            res.render("dash", {
+                                "nombre": token.nombre,
+                                "foto": token.foto,
+                                "menu": 2
+                                });
+
+        } catch (error) {
+
+            res.redirect("/")
+
+        }
+            res.render('dash')
+    }else{
+    res.redirect("/");
+    } 
+
+});
+
+dash.get('/categoria', (req,res)=>{
+
+    if (req.cookies.ckjs) {
+            
+
+        try {
+            const token = jwt.verify(req.cookies.ckjs, process.env.SECRET_KEY);
+
+            res.render("dash", {
+                                "nombre": token.nombre,
+                                "foto": token.foto,
+                                "menu": 3
+                                });
+
+        } catch (error) {
+
+            res.redirect("/")
+
+        }
+            res.render('dash')
+    }else{
+    res.redirect("/");
+    } 
+})
+
 
 dash.get('/salir', (req,res)=>{
     res.clearCookie("ckjs");
